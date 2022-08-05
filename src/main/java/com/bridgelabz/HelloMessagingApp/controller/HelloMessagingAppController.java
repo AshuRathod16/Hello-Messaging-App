4ccb4412ad74ff79ac287bbd15bdc1a5b5c4a97c
Qa-krushna <rashwini510@gmail.com>
2022-08-05 18:18:58 +0530

    Added: Ability to pass name as path variable

--- src/main/java/com/bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
+++ src/main/java/com/bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
@@ -1,20 +1,19 @@
 package com.bridgelabz.HelloMessagingApp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.HelloMessagingApp.controller.dto.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloMessagingAppController {
     @GetMapping("/getdata")
-    public String hello(){
+    public String hello() {
         return "Hello World";
     }
 
-    @GetMapping("/querry")
-    public String querryParam(@RequestParam String name){
-        return "Hello" + " " + name + " " + "from Bridgelabz";
+    @GetMapping("/path/{name}")
+    public String name(@PathVariable String name) {
+        return name;
     }
 }
+
