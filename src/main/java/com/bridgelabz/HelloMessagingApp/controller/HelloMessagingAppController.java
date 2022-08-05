1f612331e2ed2b6e699d4ee5e1c6dae5c030ebae
Qa-krushna <rashwini510@gmail.com>
2022-08-05 23:33:07 +0530

    Added: Used PUT request method & pass first name as path variable & last name as query param

--- src/main/java/com/bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
+++ src/main/java/com/bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
@@ -7,7 +7,7 @@ import com.bridgelabz.HelloMessagingApp.controller.dto.User;
 @RequestMapping("/hello")
 public class HelloMessagingAppController {
 
-    // Url : http://localhost:8083/hello/getdata
+    //Url : http://localhost:8083/hello/getdata
     @GetMapping("/getdata")
     public String hello() {
         return "Hello World";
@@ -19,14 +19,7 @@ public class HelloMessagingAppController {
         return "Hello" + " " + name + " " + "from Bridgelabz";
     }
 
-
-    // Url : http://localhost:8083/hello/path/Ashu
-    @GetMapping("/path/{name}")
-    public String name(@PathVariable String name) {
-        return name;
-    }
-
-    // Url :  http://localhost:8083/hello/adduser
+    //Url :  http://localhost:8083/hello/adduser
     @PostMapping("/adduser")
     public String adduser(@RequestBody User user) {
         return user.toString();
@@ -36,6 +29,23 @@ public class HelloMessagingAppController {
     @PostMapping("/adduserwithrequestparam")
     public String adduserwithrequestparam(@RequestParam String firstName, @RequestParam String lastName) {
         User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return user.toString();
+    }
+
+    //Url : http://localhost:8083/hello/path/Ashu
+    @GetMapping("/path/{name}")
+    public String name(@PathVariable String name) {
+        return name;
+    }
+
+    //Url : http://localhost:8083/hello/updated?firstName=Kittu&lastName=Jadhav
+    @PutMapping("/updated")
+    public String update(@RequestParam String firstName, @RequestParam String lastName) {
+        User user = new User();
+        user.setFirstName("Ashwini");
+        user.setLastName("Rathod");
         user.setFirstName(firstName);
         user.setLastName(lastName);
         return user.toString();
