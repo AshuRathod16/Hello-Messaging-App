0fe8315fe379c3e6443f5f7e7ad0b4f002f74a76
Qa-krushna <rashwini510@gmail.com>
2022-08-05 17:20:36 +0530

    Added: Ability to pass name as querry parameter

--- src/main/java/com/bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
+++ src/main/java/com/bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
@@ -2,13 +2,19 @@ package com.bridgelabz.HelloMessagingApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloMessagingAppController {
-    @GetMapping("getdata")
+    @GetMapping("/getdata")
     public String hello(){
         return "Hello World";
+    }
+
+    @GetMapping("/querry")
+    public String querryParam(@RequestParam String name){
+        return "Hello" + " " + name + " " + "from Bridgelabz";
     }
 }
