f63dcf1ade78689bf1f7c8e99b4321f43c619bf6
Qa-krushna <rashwini510@gmail.com>
2022-08-05 16:34:30 +0530

    Added: Created Rest Controller and respond hello world message to the user

---
+++ src/main/java/com/bridgelabz/HelloMessagingApp/controller/HelloMessagingAppController.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.HelloMessagingApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloMessagingAppController {
+    @GetMapping("getdata")
+    public String hello(){
+        return "Hello World";
+    }
+}
